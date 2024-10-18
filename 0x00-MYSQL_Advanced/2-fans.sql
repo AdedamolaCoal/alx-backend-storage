63a2a2e95ced645cbeb54bd484fdffda316256ad
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-18 04:39:59 +0100

    Imported data from the metal_bands table and ranks the band origins by number of non-unique fans for task 2

--- 0x00-MYSQL_Advanced/2-fans.sql
+++ 0x00-MYSQL_Advanced/2-fans.sql
@@ -1,9 +1,11 @@
--- SELECT
---   origin,
---   SUM(nb_fans) AS unique_fan_count
--- FROM
---   metal_bands
--- GROUP BY
---   origin
--- ORDER BY
---   nb_fan DESC;
+-- import data from metal_bands table
+-- and ranks the countries origins of bands by number of non unique fans
+SELECT
+  origin,
+  SUM(nb_fans) AS unique_fan_count
+FROM
+  metal_bands
+GROUP BY
+  origin
+ORDER BY
+  nb_fan DESC;

75e6585c4b1e58c36a92afcaf01f3f6a8e3d19d8
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-18 05:15:41 +0100

    Delete 0x00-MYSQL_Advanced directory

--- 0x00-MYSQL_Advanced/0-uniq_users.sql
+++
@@ -1,9 +0,0 @@
--- A script that creates a users table
--- It has three collumns: id, email, name
-CREATE TABLE
-  IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-  );

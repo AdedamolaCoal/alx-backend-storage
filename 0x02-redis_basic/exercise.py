e62c7a80ac4066c63305cabc0dd11751d09a4422
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-26 01:15:21 +0100

    task 3 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,13 +19,32 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a particular function."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"  # method.__qualname__inputs
+        output_key = f"{method.__qualname__}:outputs"  # method.__qualname__outputs
+
+        self._redis.rpush(input_key, str(args))
+
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+
+        return result
+
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """Initialize the Redis client and flush the database."""
         self._redis = redis.Redis()
-        self._redis.flushdb()  # Clear any existing data
+        self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the input data in Redis with a randomly generated key.
